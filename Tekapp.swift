88a847535b04aa29137308d1b91d759a89b3600a
Your Name <jescakw@gmail.com>
2022-11-04 18:07:05 -0500

    Just uploaded my first file

---
+++ Tekapp.swift
@@ -0,0 +1,20 @@
+// import Swift // 
+import Swift 
+
+// create variables //
+var a = "jes"
+var b = "jes"
+
+// comparing variables // 
+if (a == b)
+{
+    print("yes")
+}
+else 
+{
+    print("no")
+}
+
+/* random comment 
+I dont know what else to add 
+/*
